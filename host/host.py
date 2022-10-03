26ee8f671e6978b2d0e43b70890a61390710ff6d
Adrian Zermin <zermin.adrian@gmail.com>
2022-10-03 13:12:52 +0200

    First working sycallmonitor version

--- host/host.py
+++ host/host.py
@@ -1,12 +1,13 @@
 from controller import Controller
 from multiprocessing import Process
+from monitors.systemCallMonitor import systemCallMonitor
 
 def main():
-    with Controller() as test_controller:
-        test_controller.start_instances("./healthy/", "./infected/")
-        test_controller.execute_command("apt-get install -y iputils-ping")
-        test_controller.execute_command("ping google.com")
-        test_controller.execute_command("ps")
+    with systemCallMonitor() as systemCallmonitor:
+        with Controller() as test_controller:
+            test_controller.execute_command("apt-get install -y iputils-ping")
+            test_controller.execute_command("ping google.com")
+            test_controller.execute_command("ps")
 
 if __name__ == "__main__":
     main()
--- host/monitors/systemCallMonitor.py
+++ host/monitors/systemCallMonitor.py
@@ -1,6 +1,7 @@
 import socket
 import os
-from  subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT
+from multiprocessing import Process
 
 healthy_server_address = "/tmp/healthy_gvisor_events.sock"
 infected_server_address = "/tmp/infected_gvisor_events.sock"
@@ -8,17 +9,46 @@ infected_server_address = "/tmp/infected_gvisor_events.sock"
 healthy_configuration_file = "SecBox/host/healthy/session.json"
 infected_configuration_file = "SecBox/host/infected/session.json"
 
+base_command = "bazel run examples/seccheck:server_cc"
 
-class systemCallMonitor:
-    def __init__(self, server_adress) -> None:
-        self.server_address = server_adress
 
-    def monitoring_process(server_address):
-        server = Popen("/usr/bin/bazel run examples/seccheck:server_cc", server_address)
-        with open(server_address[5:] + "_syscalls.log") as f:
-            for line in server.stdout:
+class systemCallMonitor():
+    def __init__(self) -> None:
+        self.base_command = base_command
+
+    def run(self):
+        self.mp = Process(target=self.runInParallel, args=(self.monitoring_process, self.monitoring_process, "healthy", "infected"))
+        self.runInParallel(self.monitoring_process, self.monitoring_process,
+                           "healthy", "infected")
+
+    def __enter__(self):
+        self.run()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        for p in self.ps:
+            p.join()
+        self.mp.join()
+
+    def monitoring_process(self, infected_status):
+        cwd = os.getcwd() + "/gvisor-master/"
+        command = self.base_command + " /tmp/" + \
+            infected_status + "_gvisor_events.sock"
+        print(command)
+        # ToDo: rename self.process
+        self.process = Popen(command.split(),
+                             stdin=PIPE, stdout=PIPE, stderr=STDOUT, cwd=cwd)
+        with open(infected_status + "_syscalls.log", "w+") as f:
+            for line in self.process.stdout:
                 print(line, file=f)
                 # ToDo: Stream over Websocket
-    
 
-
+    def runInParallel(self, fn1, fn2, arg1, arg2):
+        fns = [fn1, fn2]
+        args = [arg1, arg2]
+        procs = []
+        for index in range(len(fns)):
+            p = Process(target=fns[index], args=(args[index],))
+            p.start()
+            procs.append(p)
+        self.ps = procs
