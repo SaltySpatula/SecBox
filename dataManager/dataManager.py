f18d866933c54f67c039a07f2fff4dde89ef3b39
Adrian Zermin <zermin.adrian@gmail.com>
2022-10-26 16:29:35 +0200

    First version of datamanager superclass

---
+++ dataManager/dataManager.py
@@ -0,0 +1,15 @@
+class DataManager:
+    def __init__(self, fe_client, db):
+        self.sent_packages = []
+        self.db = db
+        self.fe_client = fe_client
+        self.window = 1000
+
+    def handle_message(msg):
+        pass
+
+    def process_data(self, data):
+        pass
+
+    def save_data(self, data):
+        pass
