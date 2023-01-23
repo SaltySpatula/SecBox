32ddacc27809b1e931119efe2c9a31d75864353c
mogicacao <raffaelmogicato@gmail.com>
2023-01-23 19:14:26 +0100

    made homescreen prettier

--- app/src/components/HomeScreen.vue
+++ app/src/components/HomeScreen.vue
@@ -2,11 +2,11 @@
   <div>
     <v-container fluid>
       <v-row align="center" class="ma-2">
-        <v-col cols="12" md="4" lg="4" xl="5">
+        <v-col cols="12" md="4" lg="4" xl="4">
           <v-card class="bg-deep-purple-accent-1" style="overflow-y: auto; height:50rem">
-            <v-card-text >
+            <v-card-text style="color:black">
             <h1 class="pa-md-4" align="center"> Project Description </h1>
-            <p class="pa-md-4 text-justify">
+            <p class="text-justify">
               SecBox is a light-weight, container-based sandbox for malware analysis.
               It uses Google's <a href="https://gvisor.dev/">gVisor</a> technology to create a secure and isolate environment.
               In this environment, users can securely execute malware and compare it to a baseline container in which no such malware is executed.
@@ -18,13 +18,15 @@
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col cols="12" md="4" lg="4" xl="2" fill-height align="center"
-      justify="center">
-              <img :src="require('@/static/logo.png')" alt="SecBox Logo: An impossible cube"/>
+        <v-col cols="12" md="4" lg="4" xl="4" fill-height align="center"
+      justify="center" style="
+
+background: linear-gradient(0deg,rgba(121,86,150,0) 0%, rgba(179,136,255,0.7) 5%, rgba(225,234,236,0) 50%);">
+              <v-img :src="require('@/static/logo.png')" alt="SecBox Logo: An impossible cube"/>
         </v-col>
-        <v-col cols="12" md="4" lg="4" xl="5">
+        <v-col cols="12" md="4" lg="4" xl="4">
           <v-card class="bg-deep-purple-accent-1" style="overflow-y: auto; height:50rem" >
-            <v-card-text >
+            <v-card-text style="color:black">
             <h1 class="pa-md-4" align="center"> Latest Reports </h1>
             <v-row align="center" class="ma-2">
               <v-col justify="center" cols="4"  md="12" sm="12"
@@ -50,6 +52,7 @@
     </v-container>
   </div>
 </template>
+
 
 <script>
 
